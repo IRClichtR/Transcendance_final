154db55931e6a5d57ec6f5a89eb421afb1ad3810
balbader <basilalbapro@gmail.com>
2024-07-30 16:16:27 +0200

    update views to add new user group to DB

--- user_management/pages/views.py
+++ user_management/pages/views.py
@@ -1,7 +1,9 @@
+from readline import get_current_history_length
+from tkinter import W
 from django.shortcuts import render, redirect
 import requests
 from django.views.decorators.csrf import csrf_protect
-from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout, get_user_model
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import AppUserManager, AppUser
@@ -45,39 +47,38 @@ def index(request):
             return (render(request, 'pages/index.html', {'error': 'Differents passwords'}))
 
         try:
-            user = User.objects.create_user(
-                username=email,
-                first_name=first_name,
-                last_name=last_name,
+            user = AppUser.objects.create_user(
                 email=email,
                 password=password,
-            )
-            # user.save()
+			)
+            user.first_name = first_name
+            user.last_name = last_name
+            user.username = email
+            user.save()
             return redirect('/login')
         except Exception as e:
-            return render(request, 'pages/index.html', {'error': 'Error while creating user. Please choose others credentials.'})
+            return render(request, 'pages/index.html', {'error': e})
+            # return render(request, 'pages/index.html', {'error': 'Error while creating user. Please choose others credentials.'})
     return render(request, "pages/index.html")
 
 @csrf_protect
 def login(request):
+    user = get_user_model()
+    print('user ==========>', user)
     if request.session.get('authMethod', None) is None:
         if request.method == 'POST'  :
             email = request.POST.get('email')
+            print('email ==========>', email)
             password = request.POST.get('password')
+            print('password ==========>', password)
             user = authenticate(request, username=email, password=password)
+            print('user ==========>', user)
             if user is not None:
                 auth_login(request, user)
                 request.session['authMethod'] = 'local'
                 return redirect('/app/')
             else:
-                first_name = request.POST.get('first-name')
-                password = request.POST.get('password')
-                user = authenticate(username=first_name, password=password)
-                if user is not None:
-                    auth_login(request, user)
-                    return redirect('/app/')
-                else:
-                    return render(request, 'pages/login.html', {'error': 'Non valid credentials.'})
+                return render(request, 'pages/login.html', {'error': 'Non valid credentials.'})
         elif request.method == 'GET' and not request.user.is_authenticated:
             code = request.GET.get("code")
             if not code:
@@ -93,16 +94,7 @@ def login(request):
                 "redirect_uri": "http://localhost:8000/login"
             }
 
-            # data = {
-            #     "grant_type": "authorization_code",
-            #     "client_id": "u-s4t2ud-e6514ae93c2f3f3c25c6c98db2627ae8b9c70362848bea099f4e972c73370ec3",
-            #     "client_secret": "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",
-            #     "code": code,
-            #     "redirect_uri": "http://localhost:8000/login"
-            # }
-
             response = requests.post(url_token, headers=headers, data=data)
-            print('response==================>', response)
             if response.status_code == 200:
                 token = response.json().get("access_token")
                 if token:
@@ -116,7 +108,6 @@ def login(request):
         elif request.method == 'GET':
             return render(request, 'pages/login.html')
     else:
-        print(request.session['authMethod'])
         return render(request, 'pages/login.html', {'error': 'Already login'})
 
 #@login_required
@@ -139,6 +130,7 @@ def logout(request):
     else:
         return(logout_normal(request))
     return render(request, 'pages/login.html', {'logout': 'Error while logout'})
+
 
 def about(request):
     return render(request, "pages/about.html")
