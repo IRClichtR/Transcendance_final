1ef09d159dbd90fceb1cb54a88d587629b6fb77f
Irrlichter <ftuertinov@proton.me>
2024-08-08 10:03:24 +0200

    bootable version permission problem still on

--- pong/pythpong/pythpong/urls.py
+++ pong/pythpong/pythpong/urls.py
@@ -1,20 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import RedirectView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 from game.views import GameViewset
-""" from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
-router = routers.SimpleRouter()
-router.register('regular_game', GameViewset, basename='regular_game') """
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('game/', include('game.urls')),
+    path('pong/', include('game.urls')),
     path('', include('game.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     #simple jwt inclusion
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
